54d4c08ca8e35c20e0314194e19fa6dcd74ea689
Denis <streitmatter@informatik.uni-leipzig.de>
2021-08-16 10:56:26 +0200

    Initial code

--- src/main/kotlin/org/dbpedia/Boot.kt
+++ src/main/kotlin/org/dbpedia/Boot.kt
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 import org.springframework.stereotype.Component
+import org.semanticweb.elk.owlapi.ElkReasonerFactory
+import org.semanticweb.owlapi.apibinding.OWLManager
+import java.io.File
+
 
 @SpringBootApplication
 @Import(AsyncWorker::class)
@@ -16,6 +20,13 @@ open class ConsistencyMod
 class Processor : ModProcessor {
     override fun process(extension: Extension) {
         // TODO
+        val reasonerFactory = ElkReasonerFactory()
+        val inputHandler = OWLManager.createOWLOntologyManager()
+        val ouputHandler = OWLManager.createOWLOntologyManager()
+        val ont = inputHandler.loadOntologyFromOntologyDocument(File("./testont.owl"))
+        val reasoner = reasonerFactory.createReasoner(ont)
+        val isConsistent = reasoner.isConsistent
+        println("Ontology is consistent: $isConsistent")
         extension.setType("http://my.domain/ns#DatabusMod")
         // File resultFile = extension.createModResult();
     }
