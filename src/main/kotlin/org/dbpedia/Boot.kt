d8393bbcafd54b4d04db188ea522e0ddd5c6dfe5
Denis <streitmatter@informatik.uni-leipzig.de>
2021-08-23 14:34:29 +0200

    Adds working pom, example for each reasoner

--- src/main/kotlin/org/dbpedia/Boot.kt
+++ src/main/kotlin/org/dbpedia/Boot.kt
@@ -1,14 +1,18 @@
 package org.dbpedia
 
+import openllet.jena.PelletInfGraph
+import openllet.jena.PelletReasonerFactory
+import org.apache.jena.ontology.OntModel
+import org.apache.jena.rdf.model.ModelFactory
 import org.dbpedia.databus_mods.lib.worker.AsyncWorker
 import org.dbpedia.databus_mods.lib.worker.execution.Extension
 import org.dbpedia.databus_mods.lib.worker.execution.ModProcessor
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Import
-import org.springframework.stereotype.Component
+import org.semanticweb.HermiT.Reasoner
 import org.semanticweb.elk.owlapi.ElkReasonerFactory
 import org.semanticweb.owlapi.apibinding.OWLManager
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Import
+import org.springframework.stereotype.Component
 import java.io.File
 
 
@@ -32,7 +36,32 @@ class Processor : ModProcessor {
     }
 }
 
+fun calculate_openllet_consistency(uri: String): Boolean {
+    val model: OntModel = ModelFactory.createOntologyModel(PelletReasonerFactory.THE_SPEC)
+    model.read(uri)
+    // load the model to the reasoner
+    model.prepare()
+
+    // Get the KnolwedgeBase object
+    val kb = (model.graph as PelletInfGraph).kb
+
+    // perform initial consistency check
+    return kb.isConsistent
+}
+
 fun main(args: Array<String>) {
-    runApplication<ConsistencyMod>(*args)
+    //runApplication<ConsistencyMod>(*args)
+    val reasonerFactory = ElkReasonerFactory()
+    val inputHandler = OWLManager.createOWLOntologyManager()
+    val ont = inputHandler.loadOntologyFromOntologyDocument(File("./testont.owl"))
+    val hermitReasoner = Reasoner(ont)
+    val reasoner = reasonerFactory.createReasoner(ont)
+    val elkConsistency = reasoner.isConsistent
+    val hermitConsistency = hermitReasoner.isConsistent
+    val openlletConsistency = calculate_openllet_consistency("https://archivo.dbpedia.org/download?o=http%3A//www.w3.org/ns/shacl%23&f=ttl")
+    println("Ontology consistency: \n" +
+            "ELK: $elkConsistency\n" +
+            "OPENLLET: $openlletConsistency\n" +
+            "HermiT: $hermitConsistency\n")
 }
 
